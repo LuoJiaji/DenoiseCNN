b827e8f2837123221793448ff0c5465c69ca89ac
Luojiaji <lt920@126.com>
2019-07-03 18:46:33 +0800

    add model and training

--- demo.py
+++ demo.py
@@ -1,11 +1,11 @@
-from keras.datasets import mnist
-import matplotlib.pyplot as plt
 import numpy as np
-
+import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from keras.layers import Dense, Input
+from keras.layers.convolutional import Conv2D
+from keras.models import Model
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-
 
 alpha = 0.6
 
@@ -19,5 +19,23 @@ for i in range(len(x_train)):
     
 image_noise = np.array(image_noise)
 
-plt.imshow(image_noise[1,:,:])
-plt.imshow(x_train[1,:,:])
+
+#plt.imshow(image_noise[1,:,:])
+#plt.imshow(x_train[1,:,:])
+
+
+image_noise = np.expand_dims(image_noise, axis = 3)
+x_train = np.expand_dims(x_train, axis = 3)
+
+
+input_img = Input(shape=(28,28,1))
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv1')(input_img)
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv2')(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same', name='block1_conv3')(x)
+x = Conv2D(1, (3, 3), activation='relu', padding='same', name='block1_conv4')(x)
+model = Model(inputs = input_img, outputs = x)
+
+model.summary()
+
+model.compile(optimizer='adam', loss='mse')
+model.fit(image_noise, x_train, epochs=20, batch_size=256, shuffle=True)
