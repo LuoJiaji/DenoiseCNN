c52d5db495973c4e6841deb37df4ca829ca01ced
Luojiaji <lt920@126.com>
2019-07-18 21:26:45 +0800

    fix bug

--- DenoiseCNN_multiloss.py
+++ DenoiseCNN_multiloss.py
@@ -13,33 +13,32 @@ from keras.layers.normalization import BatchNormalization
 # 加载数据
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 alpha = 0.6
-image_noise = []
 
+x_train_noise = []
 for i in range(len(x_train)):
     img = x_train[i,:,:]
     noise = np.random.rand(28,28)
     img  = alpha*img + (1-alpha)*noise*255
-    image_noise.append(img)
-    
-image_noise = np.array(image_noise)
+    x_train_noise.append(img)
+x_train_noise = np.array(x_train_noise)
 
-#plt.figure()
-#plt.imshow(image_noise[1,:,:])
-#plt.figure()
-#plt.imshow(x_train[1,:,:])
+plt.figure()
+plt.imshow(x_train_noise[1,:,:])
+plt.figure()
+plt.imshow(x_train[1,:,:])
 
-image_noise = np.expand_dims(image_noise, axis = 3)
+x_train_noise = np.expand_dims(x_train_noise, axis = 3)
 x_train = np.expand_dims(x_train, axis = 3)
 x_test = np.expand_dims(x_test, axis = 3)
 
 x_train = x_train.astype('float32')
-image_noise = image_noise.astype('float32')
+x_train_noise = x_train_noise.astype('float32')
 x_test = x_test.astype('float32')
 
 x_train /= 255
 x_test /= 255
-y_train = np_utils.to_categorical(y_train, num_classes=10)
-y_test = np_utils.to_categorical(y_test, num_classes=10)
+y_train = np_utils.to_categorical(y_train, num_classes = 10)
+y_test = np_utils.to_categorical(y_test, num_classes = 10)
 
 
 
@@ -70,8 +69,7 @@ model_all.compile(optimizer=SGD(),
               metrics={'re_out':'mae','fc_out':'accuracy'})
 model_all.summary()
 plot_model(model_all, to_file='./model_visualization/DenoiseCNN_multiloss.png',show_shapes=True)
-model_all.fit(image_noise, [x_train, y_train], epochs=40, batch_size=64, shuffle=True)
-
+model_all.fit(x_train_noise, [x_train, y_train], epochs = 40, batch_size = 64, shuffle = True)
 
 
 #singleloss 分类模型
@@ -81,17 +79,22 @@ model_cl.compile(optimizer=SGD(),
               metrics=['accuracy'])
 model_cl.summary()
 plot_model(model_cl, to_file='./model_visualization/DenoiseCNN_classify.png', show_shapes=True)
-model_cl.fit(image_noise, y_train, epochs=40, batch_size=64, shuffle=True)
+model_cl.fit(x_train_noise, y_train, epochs = 40, batch_size = 64, shuffle = True)
+
+pre = model_cl.predict(x_test)
+pre = np.argmax(pre, axis = 1)
+label = np.argmax(y_test, axis = 1)
+acc =np.mean(pre==label)
 
 
-
-#singleloss 模型
+#singleloss 重构模型
 model_re = Model(inputs = input_img, outputs = re_out)
 model_re.compile(optimizer=SGD(), 
               loss='mse')
 model_re.summary()
 plot_model(model_re, to_file='./model_visualization/DenoiseCNN_reconstruction.png', show_shapes=True)
-model_re.fit(image_noise, x_train, epochs=40, batch_size=64, shuffle=True)
+model_re.fit(x_train_noise, x_train, epochs = 40, batch_size = 64, shuffle = True)
+
 
 
 #tmp = x_test[1]
